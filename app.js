9d13860f175292b98327e7dd29c03c07d93e6919
Phoenix <drake.mcgarrah@gmail.com>
2020-08-14 15:21:30 -0500

    Database migration

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
 var dev_db_url = "mongodb://localhost/library";
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = process.env.DB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
